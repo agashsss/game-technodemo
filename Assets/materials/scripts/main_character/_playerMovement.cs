95e7a76f64dc743ce66b38f073162710372fdb1d
sakuna <smaratov336@gmail.com>
2022-06-05 22:06:50 +0600

    technodemo

---
+++ Assets/materials/scripts/main_character/_playerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _playerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController2D _controller;
+    [SerializeField] private float speed = 35f;
+    private float direction = 0f;
+    private bool jump = false;
+
+    public Animator animator;
+
+
+
+
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        
+
+        direction = Input.GetAxisRaw("Horizontal") * speed;
+
+        animator.SetFloat("run", Mathf.Abs(direction));
+
+        if (Input.GetButton("Jump"))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    public void onLanding()
+    {
+        animator.SetBool("isJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        
+        
+        _controller.Move(direction, false,  jump);
+        
+
+
+        jump = false;
+    }
+
+   
+}
